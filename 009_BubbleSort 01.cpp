9aee63878df912b014757cd54458f270c51ff095
harlanfadhilah69 <harlancoegg@gmail.com>
2024-03-07 21:30:05 +0700

    Display

--- 009_BubbleSort 01.cpp
+++ 009_BubbleSort 01.cpp
@@ -49,3 +49,20 @@ void bubbleShortArray() {
 
 	} while (pass <= n - 1);    //step 5
 }
+void display() {
+	cout << endl;
+	cout << "============================" << endl;
+	cout << "ELement Array yang telah tersusun" << endl;
+	cout << "============================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " -->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
